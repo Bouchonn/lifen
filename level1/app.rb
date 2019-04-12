b84711a7fe2c7a56438ef4b342f7f3171f8207ac
Julien Bouchon <jul.bouchon@gmail.com>
2019-04-12 18:16:50 +0200

    level 1

--- level1/app.rb
+++ level1/app.rb
@@ -1,3 +1,67 @@
 require "json"
+require 'active_support'
+require 'active_support/core_ext'
 
-# votre code
+class RevenueGenerator
+	COM_PRICE = 0.1
+	COLOR_PRICE = 0.18
+	EXTRA_PAGE_PRICE = 0.07
+	EXPRESS_DELIVERY_PRICE = 0.6
+
+	## run with :
+	### ruby -r './app.rb' -e 'RevenueGenerator.run'
+	def self.run(data)
+		self.generate_data if data && get_data(data)
+	end
+
+	def self.get_data(data= {})
+		@practitioners = data.dig('practitioners')
+		@communications = data.dig('communications')
+		@dates = @communications.map {|c| c['sent_at'][0, 10] if c['sent_at']}.uniq.compact unless @communications.blank?
+		!(@practitioners.blank? || @communications.blank?)
+	end
+
+	def self.generate_data
+		{ 
+			'totals' =>
+				@dates.map  do |date|
+					daily_formatted_revenue(date)
+				end
+		}
+	end
+
+	def self.daily_formatted_revenue(date)
+		return nil unless date
+		{
+			'sent_on' => date,
+			'total' => daily_revenue(date)
+		}
+	end
+
+	def self.daily_revenue(date)
+		return 0.0 unless date
+		communications = @communications.select{|communication| communication['sent_at'].index(date)}
+		communications.map { |communication| communication_price communication}.sum.round(2)
+	end
+
+	def self.communication_price(communication)
+		practitioner = @practitioners.find{|practitioner| practitioner.dig('id') == communication.dig('practitioner_id')}
+		COM_PRICE + 
+		COLOR_PRICE * color(communication) +
+		EXTRA_PAGE_PRICE * extra_pages(communication) +
+		EXPRESS_DELIVERY_PRICE * express_delivery(practitioner)
+	end
+
+	def self.color(communication)
+		communication.try(:dig, 'color') ? 1 : 0
+	end
+
+	def self.extra_pages(communication)
+		[(communication.try(:dig, 'pages_number') || 1) - 1 , 0].max
+	end
+
+	def self.express_delivery(practitioner)
+		practitioner.try(:dig, 'express_delivery') ? 1 : 0
+	end
+
+end
--- level1/app_spec.rb
+++ level1/app_spec.rb
@@ -4,9 +4,96 @@ require 'rspec'
 
 require_relative './app.rb'
 
-RSpec.describe "Prices calculator", type: :model do
-  let(:data)          { JSON.parse(File.read('data.json')) }
-  let(:output)        { JSON.parse(File.read('output.json')) }
+RSpec.describe 'Revenue Generator', type: :model do
+	let(:data)          { JSON.parse(File.read('data.json')) }
+	let(:output)        { JSON.parse(File.read('output.json')) }
+	let(:corrupted_data){
+							{
+								"practitioners" =>
+								[
+							    	{
+								      "id" => 1,
+								      "first_name" => "Gregory",
+								      "last_name" => "House",
+								      "express_delivery" => true
+								    }
+							  	]
+							}
+						}
+  	def self.described_class
+  		RevenueGenerator
+	end
 
-  pending("calculates prices")
+	describe '.run' do
+		it 'returns expected data' do
+			expect(described_class.run(data)).to eq(output)
+		end
+		it 'returns nil if no input or bad input provided' do
+			expect(described_class.run(nil)).to eq(nil)
+			expect(described_class.run({})).to eq(nil)
+			expect(described_class.run(corrupted_data)).to eq(nil)
+		end
+	end
+
+	describe '.get_data' do
+		it 'returns true if data contains providers and communications, false if not' do
+			expect(described_class.get_data(data)).to eq(true)
+			expect(described_class.get_data(corrupted_data)).to eq(false)
+			expect(described_class.get_data).to eq(false)
+		end
+	end
+
+	describe '.color' do
+		colored_communication = {"id" => 1,"practitioner_id" => 1,"pages_number" => 1,"color" => true,"sent_at" => "2019-03-01 10:00:00"} 
+		uncolored_communication = {"id" => 1,"practitioner_id" => 1,"pages_number" => 1,"color" => false,"sent_at" => "2019-03-01 10:00:00"} 
+		empty_communication = {}
+		
+		it 'returns 1 if communication is colored' do
+			expect(described_class.color(colored_communication)).to eq(1)
+		end
+		it 'returns 0 if communication is not colored' do
+			expect(described_class.color(uncolored_communication)).to eq(0)
+		end
+		it 'returns 0 if no communication provided or empty communication provided' do
+			expect(described_class.color(nil)).to eq(0)
+			expect(described_class.color(empty_communication)).to eq(0)
+		end
+	end
+
+	describe '.extra_pages' do
+		no_page_communication = {"id" => 1,"practitioner_id" => 1,"pages_number" => 0,"color" => true,"sent_at" => "2019-03-01 10:00:00"} 
+		one_page_communication = {"id" => 1,"practitioner_id" => 1,"pages_number" => 1,"color" => true,"sent_at" => "2019-03-01 10:00:00"} 
+		three_pages_communication = {"id" => 1,"practitioner_id" => 1,"pages_number" => 3,"color" => true,"sent_at" => "2019-03-01 10:00:00"} 
+		empty_communication = {}
+		
+		it 'returns pages number of communication minus 1. Can t be under 0' do
+			expect(described_class.extra_pages(no_page_communication)).to eq(0)
+			expect(described_class.extra_pages(one_page_communication)).to eq(0)
+			expect(described_class.extra_pages(three_pages_communication)).to eq(2)
+		end
+		it 'returns 0 if no communication provided or empty communication provided' do
+			expect(described_class.extra_pages(nil)).to eq(0)
+			expect(described_class.extra_pages(empty_communication)).to eq(0)
+		end
+	end
+
+	describe '.express_delivery' do
+		practitioner_express = {"id" => 1,"first_name" => "Gregory","last_name" => "House","express_delivery" => true} 
+		practitioner = {"id" => 1,"first_name" => "Gregory","last_name" => "House","express_delivery" => false} 
+
+		it 'returns 1 if practitioner has express delivery' do
+			expect(described_class.express_delivery(practitioner_express)).to eq(1)
+		end
+		it 'returns 0 if practitioner has no express delivery' do
+			expect(described_class.express_delivery(practitioner)).to eq(0)
+		end
+		it 'returns 0 if no practitioner provided or empty practitioner provided' do
+			expect(described_class.express_delivery(nil)).to eq(0)
+			expect(described_class.express_delivery({})).to eq(0)
+		end
+
+	end
+
+
+
 end
